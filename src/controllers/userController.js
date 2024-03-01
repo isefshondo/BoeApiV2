db0a3b0a311bc1847574a3953a1f63a34fd7d1f5
Isabelle Hondo <isefshondo@gmail.com>
2024-02-29 21:58:54 -0300

    chore: create user route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,26 @@
-const { User: UserModel } = require('../models/User')
+const { User: UserModel, User } = require('../models/User')
 
-const userController = {}
+const userController = {
+    signUp: async(req, res) => {
+        try {
+            const user = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            }
+
+            await UserModel.collection.countDocuments({ email: user.email }, async function (err, count) {
+                if (count === 0) {
+                    const response = await UserModel.insertOne(user);
+                    res.status(201).json({ response, message: 'User created successfully' })
+                } else {
+                    res.status(400).json({ message: 'This e-mail is already being used' })
+                }
+            });
+        } catch(error) {
+            console.log(error.message)
+        }
+    }
+}
 
 module.exports = userController
