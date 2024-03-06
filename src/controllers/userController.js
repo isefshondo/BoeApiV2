4f4b53a1b08ca72f1dbdf62eb5673822937f9d6e
Isabelle Hondo <isefshondo@gmail.com>
2024-03-06 10:40:31 -0300

    refactor: signUp route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,26 +1,31 @@
 const { User: UserModel, User } = require('../models/User')
 
 const userController = {
-    signUp: async(req, res) => {
-        try {
-            const user = {
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-            }
+  signUp: async (req, res) => {
+    try {
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }
 
-            await UserModel.collection.countDocuments({ email: user.email }, async function (err, count) {
-                if (count === 0) {
-                    const response = await UserModel.insertOne(user);
-                    res.status(201).json({ response, message: 'User created successfully' })
-                } else {
-                    res.status(400).json({ message: 'This e-mail is already being used' })
-                }
-            });
-        } catch(error) {
-            console.log(error.message)
-        }
+      const doesUserAlreadyExist = await UserModel.findOne({
+        email: user.email,
+      })
+      if (doesUserAlreadyExist) {
+        return res
+          .status(400)
+          .json({ message: 'This e-mail is already being used' })
+      }
+
+      const newUser = new User(user)
+      await newUser.save()
+      res.status(201).json({ response, message: 'User created successfully' })
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ message: 'Internal server error' })
     }
+  },
 }
 
 module.exports = userController
