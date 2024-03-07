8b53e28a954e7c51e91c5b62a8e91735cdf3e80e
Isabelle Hondo <isefshondo@gmail.com>
2024-03-07 12:40:57 -0300

    chore: login route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,14 @@
 const { User: UserModel, User } = require('../models/User')
+const bcrypt = require('bcrypt')
 
 const userController = {
   signUp: async (req, res) => {
     try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
       const user = {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
       }
 
       const doesUserAlreadyExist = await UserModel.findOne({
@@ -21,6 +23,29 @@ const userController = {
       const newUser = new User(user)
       await newUser.save()
       res.status(201).json({ response, message: 'User created successfully' })
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).json({ message: 'Internal server error' })
+    }
+  },
+  signIn: async () => {
+    try {
+      const doesUserExist = await UserModel.findOne({
+        email: req.body.email,
+      })
+
+      if (!doesUserExist) {
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        doesUserExist.password,
+      )
+
+      if (!isPasswordCorrect) {
+        return res.status(401).json({ message: 'Invalid password' })
+      }
     } catch (error) {
       console.log(error.message)
       res.status(500).json({ message: 'Internal server error' })
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const userRouter = require('./user')
 
+// TODO: Create a route for every Controller file
 router.use('/', userRouter)
 
 module.exports = router
