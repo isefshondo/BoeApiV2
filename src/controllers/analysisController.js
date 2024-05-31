8cb2862338a666ed78710379c9481859c24af6cd
Isabelle Hondo <isabelle.hondo@compasso.com.br>
2024-05-31 11:49:40 -0300

    fix: sending image to AI and returning response

--- src/controllers/analysisController.js
+++ src/controllers/analysisController.js
@@ -1,10 +1,10 @@
 const { Analysis: AnalysisModel } = require('../models/Analysis');
 const path = require('path');
 const fs = require('fs-extra');
+const FormData = require('form-data');
 
 const analysisController = {
   create: async (req, res) => {
-    console.log({ file: req.file, body: req.body });
     const UPLOADS_DIR = path.join(__dirname, '../privates/uploads');
     const targetDir = path.join(UPLOADS_DIR, req.file.filename);
 
@@ -14,43 +14,45 @@ const analysisController = {
       const { animal_id } = req.body;
       const analysis_img = req.file;
 
-      console.log('Animal ID:', animal_id);
-      console.log('Image received:', analysis_img);
-
       if (!analysis_img) {
         return res.status(400).json({ message: 'Image is required.' });
       }
 
       // Send image to AI API
       const formData = new FormData();
-      formData.append('image', req.file.buffer, req.file.originalname);
+      formData.append(
+        'file',
+        fs.createReadStream(targetDir),
+        analysis_img.originalname,
+      );
 
-      //   const aiResponse = await fetch('http://:5000', {
-      //     method: 'POST',
-      //     headers: {
-      //       'Content-Type': 'multipart/form-data',
-      //     },
-      //     body: formData,
-      //   });
+      const fetch = (await import('node-fetch')).default;
+      const aiResponse = await fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        headers: formData.getHeaders(),
+        body: formData,
+      });
 
-      //   const { accuracy, predicted_class, result } = await aiResponse.json();
+      const { accuracy, predicted_class, result } = await aiResponse.json();
 
-      //   const analysis = {
-      //     animal_id,
-      //     created_at: new Date(),
-      //     disease_class: predicted_class,
-      //     accuracy,
-      //     analysis_img: req.file.buffer,
-      //     result: !result,
-      //     treatment_status: 'Sem tratamento',
-      //   };
+      const analysis = {
+        animal_id,
+        created_at: new Date(),
+        disease_class: predicted_class,
+        accuracy,
+        analysis_img: fs.readFileSync(targetDir),
+        result,
+        treatment_status: 'Sem tratamento',
+      };
 
-      //   const newAnalysis = new AnalysisModel(analysis);
-      //   await newAnalysis.save();
+      const newAnalysis = new AnalysisModel(analysis);
+      await newAnalysis.save();
 
-      //   res
-      //     .status(201)
-      //     .json({ newAnalysis, message: 'Analysis done successfully.' });
+      await fs.remove(targetDir);
+
+      res
+        .status(201)
+        .json({ newAnalysis, message: 'Analysis done successfully.' });
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: 'Internal server error.' });
