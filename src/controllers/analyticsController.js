e2dc4321a177143766f1992ce23ff243622ba869
Isabelle Hondo <isabelle.hondo@compasso.com.br>
2024-05-31 11:50:27 -0300

    fix: current positive cases percentage calculus

--- src/controllers/analyticsController.js
+++ src/controllers/analyticsController.js
@@ -1,33 +1,41 @@
-const {Animal: AnimalModel} = require('../models/Animal');
-const {Analysis: AnalysisModel} = require('../models/Analysis');
+const { Animal: AnimalModel } = require('../models/Animal');
+const { Analysis: AnalysisModel } = require('../models/Analysis');
 
 const analyticsController = {
-    getAnalytics: async (req, res) => {
-        try {
-            let positiveCases = 0, negativeCases = 0;
-            const userId = req.headers.userId;
+  getAnalytics: async (req, res) => {
+    try {
+      let positiveCases = 0,
+        negativeCases = 0;
+      const userId = req.headers.userId;
 
-            const allRegisteredAnimals = await AnimalModel.find({user_id: userId});
+      const allRegisteredAnimals = await AnimalModel.find({ user_id: userId });
 
-            for (let i = 0; i < allRegisteredAnimals.length; i++) {
-                const getCowAnalysis = await AnalysisModel.find({animal_id: allRegisteredAnimals[i]['_id']});
-                positiveCases = getCowAnalysis.filter((data) => data.result === true).length + positiveCases;
-                negativeCases = getCowAnalysis.filter((data) => data.result === false).length + negativeCases;
-            }
+      for (let i = 0; i < allRegisteredAnimals.length; i++) {
+        const getCowAnalysis = await AnalysisModel.find({
+          animal_id: allRegisteredAnimals[i]['_id'],
+        });
+        positiveCases =
+          getCowAnalysis.filter((data) => data.result === true).length +
+          positiveCases;
+        negativeCases =
+          getCowAnalysis.filter((data) => data.result === false).length +
+          negativeCases;
+      }
 
-            const currentPositiveCasesPercentage = (positiveCases / allRegisteredAnimals) * 100;
+      const currentPositiveCasesPercentage =
+        (positiveCases / allRegisteredAnimals.length) * 100;
 
-            res.status(200).json({
-                registered_animals: allRegisteredAnimals.length,
-                current_positive_cases_percentage: currentPositiveCasesPercentage,
-                current_positive_cases: positiveCases,
-                current_negative_cases: negativeCases,
-            });
-        } catch (error) {
-            console.log(error.message);
-            res.status(500).json({message: "Internal server error"})
-        }
-    },
+      res.status(200).json({
+        registered_animals: allRegisteredAnimals.length,
+        current_positive_cases_percentage: currentPositiveCasesPercentage,
+        current_positive_cases: positiveCases,
+        current_negative_cases: negativeCases,
+      });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  },
 };
 
 module.exports = analyticsController;
